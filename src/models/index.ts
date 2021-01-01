39bb350e6d7500b1c1a9aa03f8dff073af85d61e
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-01-02 01:29:45 +0900

    UPDATE; add database relation

--- src/models/index.ts
+++ src/models/index.ts
@@ -17,6 +17,23 @@ const createModels = (): DbInterface => {
     Supply: SupplyFactory(sequelize),
     Option: OptionFactory(sequelize)
   };
+
+  // club - tag relation n:m
+  db.Club.belongsToMany(db.Tag, { foreignKey: "club_id", through: "club_tag" });
+  db.Tag.belongsToMany(db.Club, { foreignKey: "tag_id", through: "club_tag" });
+
+  // user - club relation 1:n
+  db.Club.hasMany(db.User, { sourceKey: "id", foreignKey: "club_id" });
+  db.User.belongsTo(db.Club, { targetKey: "id", foreignKey: "club_id" });
+
+  // user - supply relation 1:n
+  db.User.hasMany(db.Supply, { sourceKey: "id", foreignKey: "user_id" });
+  db.Supply.belongsTo(db.User, { targetKey: "id", foreignKey: "user_id" });
+
+  // supply - option relation 1:n
+  db.Supply.hasMany(db.Option, { sourceKey: "id", foreignKey: "supply_id" });
+  db.Option.belongsTo(db.Supply, { targetKey: "id", foreignKey: "supply_id" });
+
   return db;
 }
 
