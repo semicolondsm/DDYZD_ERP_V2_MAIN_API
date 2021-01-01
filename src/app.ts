df4037a79721ebe906912b70f0bbae27467f4098
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-01-01 19:37:14 +0900

    UPDATE; add dotenv module

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,12 @@ import express, { NextFunction, Request, Response } from "express";
 import bodyParser from "body-parser";
 import morgan from "morgan";
 import cors from "cors";
+import dotenv from "dotenv";
 import router from "./routes";
 
 const app: express.Application = express();
+
+dotenv.config();
 
 app.use(morgan("dev"));
 app.use(bodyParser.json());
