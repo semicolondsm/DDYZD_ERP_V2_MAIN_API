bcbd619cd648b6c92f5763ff3617428f0a53de3c
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-01-02 00:26:43 +0900

    UPDATE; add supply model interface

--- src/models/objectRelationalMapping/db.interface.ts
+++ src/models/objectRelationalMapping/db.interface.ts
@@ -1,5 +1,10 @@
 import Sequelize from "sequelize";
-import { ClubInterface, ClubTagInterface, TagInterface, UserInterface } from "./model.interfaces";
+import { 
+  ClubInterface, 
+  ClubTagInterface, 
+  SupplyInterface, 
+  TagInterface, 
+  UserInterface } from "./model.interfaces";
 
 export interface DbInterface {
   sequelize: Sequelize.Sequelize;
@@ -7,4 +12,5 @@ export interface DbInterface {
   Tag: Sequelize.ModelCtor<TagInterface>;
   ClubTag: Sequelize.ModelCtor<ClubTagInterface>;
   User: Sequelize.ModelCtor<UserInterface>;
+  Supply: Sequelize.ModelCtor<SupplyInterface>;
 }
--- src/models/objectRelationalMapping/model.interfaces.ts
+++ src/models/objectRelationalMapping/model.interfaces.ts
@@ -1,6 +1,7 @@
 import Sequelize from "sequelize";
 import { ClubAttributes } from "../clubModel/attributes";
 import { ClubTagAttributes } from "../clubTagModel/attributes";
+import { SupplyAttributes } from "../supplyModel/attributes";
 import { TagAttributes } from "../tagModel/attributes";
 import { UserAttributes } from "../userModel/attributes";
 
@@ -8,10 +9,12 @@ interface ClubInterface extends Sequelize.Model<ClubAttributes, ClubAttributes>,
 interface TagInterface extends Sequelize.Model<TagAttributes, TagAttributes>, TagAttributes {}
 interface ClubTagInterface extends Sequelize.Model<ClubTagAttributes, ClubTagAttributes>, ClubTagAttributes {} 
 interface UserInterface extends Sequelize.Model<UserAttributes, UserAttributes>, UserAttributes {}
+interface SupplyInterface extends Sequelize.Model<SupplyAttributes, SupplyAttributes>, SupplyAttributes {}
 
 export {
   ClubInterface, 
   TagInterface,
   ClubTagInterface,
-  UserInterface
+  UserInterface,
+  SupplyInterface
 }
