08bc4c1128a1cda5a36be9f4e865819f1c840318
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-01-01 20:21:16 +0900

    UPDATE; add tag model interface

--- src/models/objectRelationalMapping/db.interface.ts
+++ src/models/objectRelationalMapping/db.interface.ts
@@ -1,7 +1,8 @@
 import Sequelize from "sequelize";
-import { ClubInterface } from "./model.interfaces";
+import { ClubInterface, TagInterface } from "./model.interfaces";
 
 export interface DbInterface {
   sequelize: Sequelize.Sequelize;
   Club: Sequelize.ModelCtor<ClubInterface>;
+  Tag: Sequelize.ModelCtor<TagInterface>;
 }
--- src/models/objectRelationalMapping/model.interfaces.ts
+++ src/models/objectRelationalMapping/model.interfaces.ts
@@ -1,8 +1,11 @@
 import Sequelize from "sequelize";
 import { ClubAttributes } from "../clubModel/attributes";
+import { TagAttributes } from "../tagModel/attributes";
 
 interface ClubInterface extends Sequelize.Model<ClubAttributes, ClubAttributes>, ClubAttributes {}
+interface TagInterface extends Sequelize.Model<TagAttributes, TagAttributes>, TagAttributes {}
 
 export {
-  ClubInterface
+  ClubInterface, 
+  TagInterface
 }
