03cfd587d2dc3015b66b069c05a4bf6289ca5bab
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-01-01 20:04:10 +0900

    CREATE; club model

---
+++ src/models/clubModel/attributes.ts
@@ -0,0 +1,8 @@
+interface ClubAttributes {
+  id: number;
+  name: string;
+  total_budget: number;
+  current_budget: number; 
+}
+
+export { ClubAttributes }
---
+++ src/models/clubModel/clubFactory.ts
@@ -0,0 +1,31 @@
+import Sequelize, { INTEGER, STRING } from "sequelize";
+import { ClubInterface } from "../objectRelationalMapping/model.interfaces";
+import { SequelizeAttributes } from "../objectRelationalMapping/sequelize.attributes";
+import { ClubAttributes } from "./attributes";
+
+const ClubFactory = (sequelize: Sequelize.Sequelize): Sequelize.ModelCtor<ClubInterface> => {
+  const attributes: SequelizeAttributes<ClubAttributes> = {
+    id: {
+      type: INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    }, 
+    name: {
+      type: STRING,
+      allowNull: false,
+    }, 
+    total_budget: {
+      type: INTEGER,
+    },
+    current_budget: {
+      type: INTEGER,
+    },
+  };
+  const Club = sequelize.define<ClubInterface, ClubAttributes>("club", attributes, {
+    timestamps: false,
+  });
+  return Club;
+}
+
+export { ClubFactory }
