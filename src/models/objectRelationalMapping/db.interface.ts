9aac2ed8689a68780f7a91c2ab50cbe5a17c6f72
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-01-01 20:04:52 +0900

    UPDATE; add club model interface

--- src/models/objectRelationalMapping/db.interface.ts
+++ src/models/objectRelationalMapping/db.interface.ts
@@ -1,5 +1,7 @@
 import Sequelize from "sequelize";
+import { ClubInterface } from "./model.interfaces";
 
 export interface DbInterface {
   sequelize: Sequelize.Sequelize;
+  Club: Sequelize.ModelCtor<ClubInterface>;
 }
--- src/models/objectRelationalMapping/model.interfaces.ts
+++ src/models/objectRelationalMapping/model.interfaces.ts
@@ -1,5 +1,8 @@
 import Sequelize from "sequelize";
+import { ClubAttributes } from "../clubModel/attributes";
+
+interface ClubInterface extends Sequelize.Model<ClubAttributes, ClubAttributes>, ClubAttributes {}
 
 export {
-
+  ClubInterface
 }
