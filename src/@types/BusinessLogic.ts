a5f429d0fab4c2b21df1d6f48767b3cc2f24ae52
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-01-02 20:19:54 +0900

    CREATE; Business Logic Interface

---
+++ src/@types/BusinessLogic.ts
@@ -0,0 +1,20 @@
+import { Request, Response, NextFunction } from "express"
+
+interface CustomRequest extends Request {
+  [key: string]: any;
+}
+
+interface CustomResponse extends Response {
+  [key: string]: any;
+}
+
+interface BusinessLogic {
+  (req: CustomRequest, res: CustomResponse, next: NextFunction): any;
+}
+
+export {
+  CustomRequest, 
+  CustomResponse, 
+  NextFunction, 
+  BusinessLogic
+}
