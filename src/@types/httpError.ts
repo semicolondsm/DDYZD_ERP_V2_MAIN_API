37b715203f239d05ca0913d734456ef1c1f51954
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-01-02 20:20:23 +0900

    CREATE; custom error class

---
+++ src/@types/httpError.ts
@@ -0,0 +1,10 @@
+class HttpError extends Error {
+  public readonly status: number;
+  public readonly message: string;
+  
+  constructor(status: number, message: string) {
+    super(message);
+    this.status = status;
+    this.message = message;
+  }
+}
