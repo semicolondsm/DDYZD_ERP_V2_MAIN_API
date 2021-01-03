3a8f5bae6f181ddcb5739eff6dd436080a5d9a7a
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-01-03 21:45:47 +0900

    UPDATE; add supply item api

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,12 @@
 import { Router } from 'express';
+import { BusinessLogic } from '../@types/BusinessLogic';
+import { supplyClubItems } from '../controllers/clubRelatedApi';
+import { errorHandler } from '../middlewares/errorHandler';
 
 const router: Router = Router();
+
+const supplyClubItemsHandler: BusinessLogic = errorHandler(supplyClubItems);
+
+router.post("/club/:club_id/supply", supplyClubItemsHandler);
 
 export default router;
