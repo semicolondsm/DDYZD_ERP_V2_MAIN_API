5e0a21897e43cd6cc540f267276023091cf9bcd3
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-01-03 21:54:08 +0900

    REFACTOR; add verity token middleware

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,12 @@ import { Router } from 'express';
 import { BusinessLogic } from '../@types/BusinessLogic';
 import { supplyClubItems } from '../controllers/clubRelatedApi';
 import { errorHandler } from '../middlewares/errorHandler';
+import { verifyToken } from '../middlewares/verifyToken';
 
 const router: Router = Router();
 
 const supplyClubItemsHandler: BusinessLogic = errorHandler(supplyClubItems);
 
-router.post("/club/:club_id/supply", supplyClubItemsHandler);
+router.post("/club/:club_id/supply", verifyToken, supplyClubItemsHandler);
 
 export default router;
