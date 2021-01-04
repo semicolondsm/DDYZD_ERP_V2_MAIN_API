fa8ff0589a9a48461c5ae368d3b341dd517feb98
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-01-04 12:25:57 +0900

    REFACTOR; express middleware

---
+++ src/loaders/express.ts
@@ -0,0 +1,32 @@
+import express, { Request, Response, NextFunction } from "express";
+import bodyParser from "body-parser";
+import morgan from "morgan";
+import cors from "cors";
+import router from "../routes";
+
+const loadExpressMiddleware = (app: express.Application) => {
+  app.use(morgan("dev"));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    const allowOrigins: string[] = [process.env.ALLOW_ORIGINS1, process.env.ALLOW_ORIGINS2];
+    const origin: string = req.headers.origin;
+    if(allowOrigins.includes(origin)) {
+      return cors({
+        origin,
+        credentials: true,
+      })(req, res, next);
+    } else {
+      next();
+    }
+  });
+  
+  app.use("/", router);
+  
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    res.status(404).send("Sorry, cant find that");
+  });
+}
+
+export { loadExpressMiddleware }
