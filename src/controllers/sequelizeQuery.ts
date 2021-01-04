46e43ecb10bb3daf789e74a2d3831327127325ee
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-01-04 19:21:51 +0900

    REFACTOR; query function

--- src/controllers/sequelizeQuery.ts
+++ src/controllers/sequelizeQuery.ts
@@ -1,5 +1,5 @@
 import { db } from "../models";
-import { ClubInterface, OptionInterface, SupplyInterface } from "../models/objectRelationalMapping/model.interfaces";
+import * as SequelizeInterface from "../models/objectRelationalMapping/model.interfaces";
 
 const InsertSupplyData = (
   name: string,
@@ -9,24 +9,27 @@ const InsertSupplyData = (
   link: string,
   user_id: number,
   club_id: number)
-  : Promise<SupplyInterface> => {
+  : Promise<SequelizeInterface.SupplyInterface> => {
   return db.Supply.create({ name, price, status, count, link, user_id, club_id, });
 }
 
-const InsertOptionData = (option: string, supply_id: number): Promise<OptionInterface> => {
+const InsertOptionData = (option: string, supply_id: number): 
+Promise<SequelizeInterface.OptionInterface> => {
   return db.Option.create({
     script: option,
     supply_id: supply_id
   });
 }
 
-const FindSupplyById = (supply_id: number): Promise<SupplyInterface> => {
+const FindSupplyById = (supply_id: number): 
+Promise<SequelizeInterface.SupplyInterface> => {
   return db.Supply.findOne({
     where: { id: supply_id },
   });
 }
 
-const FindClubById = (club_id: number): Promise<ClubInterface> => {
+const FindClubById = (club_id: number): 
+Promise<SequelizeInterface.ClubInterface> => {
   return db.Club.findOne({
     where: { id: club_id },
   });
