a864b9ee51e249eb791c396ec5cce05701e7b8f5
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-01-04 20:27:25 +0900

    UPDATE; add put items router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,15 @@
 import { Router } from 'express';
 import { BusinessLogic } from '../@types/BusinessLogic';
-import { supplyClubItems } from '../controllers/clubRelatedApi';
+import * as mainController from '../controllers/clubRelatedApi';
 import { errorHandler } from '../middlewares/errorHandler';
 import { verifyToken } from '../middlewares/verifyToken';
 
 const router: Router = Router();
 
-const supplyClubItemsHandler: BusinessLogic = errorHandler(supplyClubItems);
+const supplyClubItemsHandler: BusinessLogic = errorHandler(mainController.supplyClubItems);
+const putClubItemsHandler: BusinessLogic = errorHandler(mainController.putClubItems);
 
 router.post("/club/:club_id/supply", verifyToken, supplyClubItemsHandler);
+router.put("/club/:club_id/supply/:supply_id", verifyToken, putClubItemsHandler);
 
 export default router;
