51d74e90523278f9a25f505dda3d524535f20dba
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-01-04 19:16:00 +0900

    REFACTOR; query module

--- src/controllers/clubRelatedApi.ts
+++ src/controllers/clubRelatedApi.ts
@@ -1,19 +1,19 @@
 import { BusinessLogic } from "../@types/BusinessLogic";
 import { HttpError } from "../@types/httpError";
 import { ClubInterface, SupplyInterface } from "../models/objectRelationalMapping/model.interfaces";
-import { findClubByIdQuery, findSupplyByIdQuery, insertOptionDataQuery, insertSupplyDataQuery } from "./sequelizeQuery";
+import * as Query from "./sequelizeQuery";
 
 const supplyClubItems: BusinessLogic = async (req, res, next) => {
   const { price, name, count, option, url } = req.body as { price: number, name: string, count: number, option: string, url: string };
   const club_id: number = Number(req.params.club_id);
-  const majorClub: ClubInterface = await findClubByIdQuery(club_id);
+  const majorClub: ClubInterface = await Query.FindClubById(club_id);
   if(majorClub.current_budget - price < 0) {
     throw new HttpError(400, "예산 초과");
   }
-  const supply: SupplyInterface = await insertSupplyDataQuery(
+  const supply: SupplyInterface = await Query.InsertSupplyData(
     name, price, 2, count, url, req.decoded.user_id, club_id,
   );
-  insertOptionDataQuery(option, supply.id);
+  Query.InsertOptionData(option, supply.id);
   res.status(200).json({
     msg: "success",
   });
@@ -23,11 +23,11 @@ const putClubItems: BusinessLogic = async (req, res, next) => {
   const club_id: number = Number(req.params.club_id);
   const supply_id: number = Number(req.params.supply_id);
   const { count, price } = req.body as { count: number, price: number };
-  const supply: SupplyInterface = await findSupplyByIdQuery(supply_id);
+  const supply: SupplyInterface = await Query.FindSupplyById(supply_id);
   if(supply.club_id !== club_id || supply.user_id !== req.decoded.user_id) {
     throw new HttpError(403, "접근 권한이 없습니다.");
   }
-  const majorClub = await findClubByIdQuery(club_id);
+  const majorClub = await Query.FindClubById(club_id);
   if(majorClub.current_budget - price < 0) {
     throw new HttpError(400, "예산 초과");
   }
