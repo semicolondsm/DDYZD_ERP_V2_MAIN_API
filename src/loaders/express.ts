69cb4fdfad3af6dbe1036f469c5611506c391bf4
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-01-08 03:54:48 +0900

    UPDATE; install helmet, hpp

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response, NextFunction } from "express";
 import bodyParser from "body-parser";
+import helmet from "helmet";
+import hpp from "hpp";
 import morgan from "morgan";
 import cors from "cors";
 import router from "../routes";
@@ -8,7 +10,10 @@ const loadExpressMiddleware = (app: express.Application) => {
   app.use(morgan("dev"));
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }));
-  
+  if(process.env.NODE_ENV === "production") {
+    app.use(helmet());
+    app.use(hpp());
+  }
   app.use((req: Request, res: Response, next: NextFunction) => {
     const allowOrigins: string[] = [process.env.ALLOW_ORIGINS1, process.env.ALLOW_ORIGINS2];
     const origin: string = req.headers.origin;
