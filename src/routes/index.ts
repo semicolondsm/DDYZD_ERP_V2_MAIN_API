8925bf9a723293e3af9e5a8a432f3eadf5fa5641
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-01-07 15:26:04 +0900

    UPDATE; add refresh token router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,16 +4,19 @@ import * as mainController from '../controllers/clubRelatedApi';
 import * as authController from "../controllers/authenticationUserApi";
 import { errorHandler } from '../middlewares/errorHandler';
 import { verifyToken } from '../middlewares/verifyToken';
+import { verifyRefreshToken } from '../middlewares/verifyRefreshToken';
 
 const router: Router = Router();
 
 const provideTokenHandler: BusinessLogic = errorHandler(authController.provideToken);
+const refreshTokenHandler: BusinessLogic = errorHandler(authController.refreshToken);
 
 const supplyClubItemsHandler: BusinessLogic = errorHandler(mainController.supplyClubItems);
 const putClubItemsHandler: BusinessLogic = errorHandler(mainController.putClubItems);
 const deleteClubItemsHandler: BusinessLogic = errorHandler(mainController.deleteClubItems);
 
 router.get("/user/token", provideTokenHandler);
+router.get("/user/refresh", verifyRefreshToken, refreshTokenHandler);
 
 router.post("/club/:club_id/supply", verifyToken, supplyClubItemsHandler);
 router.put("/club/:club_id/supply/:supply_id", verifyToken, putClubItemsHandler);
