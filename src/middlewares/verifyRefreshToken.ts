e272dda6fe56c69f9df1aba3b144a509d427b595
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-01-07 15:10:49 +0900

    CREATE; verify refresh token middleware

---
+++ src/middlewares/verifyRefreshToken.ts
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken";
+import { BusinessLogic } from "../@types/BusinessLogic";
+import { HttpError } from "../@types/httpError";
+import { errorHandler } from "./errorHandler";
+
+const verifyRefreshTokenLogic: BusinessLogic = (req, res, next) => {
+  try {
+    const refreshToken: string = req.headers["refresh-token"] as string;
+    if(!refreshToken) {
+      throw new Error();
+    } 
+    const rt_decoded = jwt.verify(refreshToken, process.env.JWT_SECRET);
+    if(rt_decoded !== "refresh") {
+      throw new Error();
+    }
+    req.rt_decoded = rt_decoded;
+    next();
+  } catch(err) {
+    if(err.message === "TokenExpiredError") {
+      throw new HttpError(401, "Expired token");
+    } else {
+      throw new HttpError(401, "Unauthorized token");
+    }
+  }
+}
+
+const verifyRefreshToken: BusinessLogic = errorHandler(verifyRefreshTokenLogic);
+
+export { verifyRefreshToken }
