9124512bc71786ef84ae0937c7af002d0ce02936
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-01-07 14:58:19 +0900

    UPDATE; add provide token router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,19 @@
 import { Router } from 'express';
 import { BusinessLogic } from '../@types/BusinessLogic';
 import * as mainController from '../controllers/clubRelatedApi';
+import * as authController from "../controllers/authenticationUserApi";
 import { errorHandler } from '../middlewares/errorHandler';
 import { verifyToken } from '../middlewares/verifyToken';
 
 const router: Router = Router();
 
+const provideTokenHandler: BusinessLogic = errorHandler(authController.provideToken);
+
 const supplyClubItemsHandler: BusinessLogic = errorHandler(mainController.supplyClubItems);
 const putClubItemsHandler: BusinessLogic = errorHandler(mainController.putClubItems);
 const deleteClubItemsHandler: BusinessLogic = errorHandler(mainController.deleteClubItems);
+
+router.get("/user/token", provideTokenHandler);
 
 router.post("/club/:club_id/supply", verifyToken, supplyClubItemsHandler);
 router.put("/club/:club_id/supply/:supply_id", verifyToken, putClubItemsHandler);
