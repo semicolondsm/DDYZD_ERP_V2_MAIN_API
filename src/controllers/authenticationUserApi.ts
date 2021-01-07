c540d7d2e294e8847097545797b4b64bd4bf8be4
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-01-07 17:33:39 +0900

    FIX; handle axios error

--- src/controllers/authenticationUserApi.ts
+++ src/controllers/authenticationUserApi.ts
@@ -4,22 +4,16 @@ import { HttpError } from "../@types/httpError";
 import { db } from "../models";
 import { UserInterface } from "../models/objectRelationalMapping/model.interfaces";
 import { UserInfo } from "./dto/userInfo";
-import { issuanceAccessToken, issuanceRefreshToken } from "./functions/issuanceToken";
-
-const DSM_AUTH_URL = "http://54.180.98.91:8090";
+import { getUserInfo, issuanceAccessToken, issuanceRefreshToken } from "./functions/userAuthentication";
 
 const provideToken: BusinessLogic = async (req, res, next) => {
   const token: string = req.headers["access-token"] as string;
   if(!token) {
     throw new HttpError(400, "Bad Request");
   }
-  const response: AxiosResponse<UserInfo> = await axios.get(`${DSM_AUTH_URL}/v1/info/basic`, {
-    headers: {
-      "access-token": `Bearer ${token}`,
-    }
-  });
+  const userInfo: UserInfo = await getUserInfo(token);
   const user: [UserInterface, boolean] = await db.User.findOrCreate({ 
-    where: response.data,
+    where: userInfo,
   });
   const accessToken: string = await issuanceAccessToken(user[0].id);
   const refreshToken: string = await issuanceRefreshToken(user[0].id);
